5a5de3e58eae30c4f33b81c600880089e21a7f7f
Yoiset Abrahan <yoisetabrahan@gmail.com>
2022-07-24 12:41:47 -0400

    Adding pages ans exporting

---
+++ src/pages/MyFaveNews/MyFaveNews.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyFaveNews = () => {
+    return <div>MyFaveNews</div>;
+};
+
+export default MyFaveNews;
---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as AllNews } from './AllNews/AllNews';
+export { default as MyFaveNews } from './MyFaveNews/MyFaveNews';
