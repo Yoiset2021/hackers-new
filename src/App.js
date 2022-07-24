924b1e259ac8f7fd31022307f467bb2cc9f15170
Yoiset Abrahan <yoisetabrahan@gmail.com>
2022-07-24 12:43:48 -0400

    Adding router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Layout } from './components';
+import { AllNews, MyFaveNews } from './pages';
+import { HOME, MY_FAVES } from './constants/routes';
+
 const App = () => {
-    return <div className="App">Hacker News</div>;
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path={HOME} element={<AllNews />} />
+                    <Route path={`/${MY_FAVES}`} element={<MyFaveNews />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
 };
 
 export default App;
